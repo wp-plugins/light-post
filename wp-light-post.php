75e39128ee8371c33fd86d55058707dc963e2d93
M66B <M66B@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-28 06:25:24 +0000

    1.4 release
    git-svn-id: https://plugins.svn.wordpress.org/light-post/tags/1.4@416259 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-light-post.php
+++ wp-light-post.php
@@ -3,7 +3,7 @@
 Plugin Name: Light Post
 Plugin URI: http://blog.bokhorst.biz/4146/computers-en-internet/wordpress-plugin-light-post/
 Description: Resource light alternate post management screen for use on slow connections and/or slow computers
-Version: 1.0
+Version: 1.4
 Author: Marcel Bokhorst
 Author URI: http://blog.bokhorst.biz/about/
 */
@@ -45,6 +45,8 @@ if (basename($lp_request_uri) == basename(__FILE__) && isset($_REQUEST['abspath'
 	// Create WordPress environment
 	require_once(urldecode($_REQUEST['abspath']) . 'wp-load.php');
 	require_once(urldecode($_REQUEST['abspath']) . 'wp-admin/includes/admin.php');
+	require_once(urldecode($_REQUEST['abspath']) . '/wp-admin/includes/plugin.php');
+	require_once(urldecode($_REQUEST['abspath']) . WPINC . '/pluggable.php');
 
 	// Handle request
 	WPLightPost::Handle_request();
