6522c35ffa10efcbf302b06422bd0f43a4161e48
M66B <M66B@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-02 14:25:11 +0000

    1.7 release
    git-svn-id: https://plugins.svn.wordpress.org/light-post/trunk@483415 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-light-post.php
+++ wp-light-post.php
@@ -3,7 +3,7 @@
 Plugin Name: Light Post
 Plugin URI: http://blog.bokhorst.biz/4146/computers-en-internet/wordpress-plugin-light-post/
 Description: Resource light alternate post management screen for use on slow connections and/or slow computers
-Version: 1.6
+Version: 1.7
 Author: Marcel Bokhorst
 Author URI: http://blog.bokhorst.biz/about/
 */
